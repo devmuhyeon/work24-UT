23bb80fcae3931f18702947034d5e861c5dcd2fa
김무현 <dev.muhyeon@gmail.com>
2026-06-04 16:38:22 +0900

    ㅁㅁ

--- js/alert.js
+++ js/alert.js
@@ -1,13 +1,45 @@
 $(function () {
 
-    history.pushState(null, null, location.href);
+	var isMobile =
+		/Android|iPhone|Mobile/i.test(
+			navigator.userAgent
+		);
 
-    $(window).on('popstate', function () {
+	if (isMobile) {
 
-        history.pushState(null, null, location.href);
+		$('body').html(
+			'<main style="padding:40px;text-align:center;">' +
+				'<h1>PC 환경에서만 참여 가능합니다.</h1>' +
+				'<p>모바일에서는 참여할 수 없습니다.</p>' +
+			'</main>'
+		);
 
-        alert('테스트 진행중에 뒤로가기를 사용할 수 없습니다.');
+		return;
 
-    });
+	}
+
+	for (var i = 0; i < 3; i++) {
+
+		history.pushState(
+			null,
+			'',
+			location.href
+		);
+
+	}
+
+	$(window).on('popstate', function () {
+
+		history.pushState(
+			null,
+			'',
+			location.href
+		);
+
+		alert(
+			'테스트 진행중에는 뒤로가기를 사용할 수 없습니다.'
+		);
+
+	});
 
 });
