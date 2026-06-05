dfccd2cae407c0e283cd32f4c93e053eba638533
김무현 <dev.muhyeon@gmail.com>
2026-06-05 09:27:23 +0900

    ddd

--- js/monon.js
+++ js/monon.js
@@ -1,4 +1,4 @@
-(function () {
+$(function () {
 
 	var isMobile =
 		/Android|iPhone|iPad|iPod|Mobile|Windows Phone|webOS|BlackBerry/i.test(
@@ -9,15 +9,8 @@
 		return;
 	}
 
-	document.documentElement.innerHTML =
-		'<head>' +
-			'<meta charset="utf-8">' +
-			'<title>접속 제한</title>' +
-		'</head>' +
-		'<body>' +
-			'<main style="padding:40px;text-align:center;">' +
-				'<h1>PC 환경에서만 참여 가능합니다.</h1>' +
-			'</main>' +
-		'</body>';
+	$('body').html(
+		'<p style="padding:40px;text-align:center;">PC 환경에서만 참여 가능합니다.</p>'
+	);
 
-})();
+});
