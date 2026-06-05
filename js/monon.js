902f93c13618c868c0abe7b1ae1ed0049a751e2e
김무현 <dev.muhyeon@gmail.com>
2026-06-05 09:38:19 +0900

    aa

--- js/monon.js
+++ js/monon.js
@@ -24,15 +24,15 @@
 				'line-height:1.8;' +
 			'">' +
 
-				'<h1 style="margin:0 0 24px;font-size:24px;font-weight:700;">PC 환경에서만 참여 가능합니다.</h1>' +
+				'<h1 style="margin:0 0 24px;font-size:20px;font-weight:700;">PC 환경에서만 참여 가능합니다.</h1>' +
 
-				'<p style="margin:0 0 16px;font-size:18px;">현재 진행되는 사용자 경험 조사는 PC 화면 기준으로 제작되어 있습니다.</p>' +
+				'<p style="margin:0 0 16px;font-size:14px; color: #757575;">현재 진행되는 사용자 경험 조사는 PC 화면 기준으로 제작되어 있습니다.</p>' +
 
-				'<p style="margin:0 0 12px;font-size:16px;">모바일 또는 태블릿으로 참여할 경우 화면 구성 및 메뉴 구조가 다르게 표시되어 조사 결과에 영향을 줄 수 있습니다.</p>' +
+				'<p style="margin:0 0 12px;font-size:14px; color: #757575;">모바일 또는 태블릿으로 참여할 경우 화면 구성 및 메뉴 구조가 다르게 표시되어 조사 결과에 영향을 줄 수 있습니다.</p>' +
 
-				'<p style="margin:0 0 12px;font-size:16px;">정확한 조사 진행을 위해 PC(데스크탑 또는 노트북) 환경에서 다시 접속해 주시기 바랍니다.</p>' +
+				'<p style="margin:0 0 12px;font-size:14px; color: #757575;">정확한 조사 진행을 위해 PC(데스크탑 또는 노트북) 환경에서 다시 접속해 주시기 바랍니다.</p>' +
 
-				'<p style="margin:0;font-size:16px;">감사합니다.</p>' +
+				'<p style="margin:0;font-size:14px; color: #757575;">감사합니다.</p>' +
 
 			'</main>';
 
